b1cca1dd9e333af74a7435143ae82f4a8bf3f49e
caiomilanic <caiomilanic@gmail.com>
2026-08-11 22:07:16 -0300

    Update cors policy

--- landing-api/src/main/java/com/estudioveneto/landing_api/config/CorsConfig.java
+++ landing-api/src/main/java/com/estudioveneto/landing_api/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173", "https://estudios-veneto-frontend.vercel.app/")
+                .allowedOriginPatterns("http://localhost:5173", "https://*.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
