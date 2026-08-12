a68daa5b844ce9d835951a36441b3785eb834ffb
caiomilanic <caiomilanic@gmail.com>
2026-08-11 21:55:29 -0300

    Update endereço frontend para deployment Vercel

--- landing-api/src/main/java/com/estudioveneto/landing_api/config/CorsConfig.java
+++ landing-api/src/main/java/com/estudioveneto/landing_api/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173", "https://estudio-veneto.vercel.app")
+                .allowedOrigins("http://localhost:5173", "https://estudios-veneto-frontend.vercel.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
     }
